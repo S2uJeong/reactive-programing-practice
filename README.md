10bb02a755682b5f706d271932043644ed2d5300
sujeong <sj980204@gmail.com>
2025-09-08 23:32:39 +0900

    [feat] Operators 실습(1)

---
+++ README.md
@@ -0,0 +1,41 @@
+# Reactive Programing
+## Operators
+- PubSub.java
+
+## Others
+### 아래 코드에서 Throwable  대신 Exception을 넣는 것의 차이
+
+```java
+@Override
+public void subscribe(Subscriber<? super Integer> sub) { // Sub : 데이터 받는자
+    sub.onSubscribe(new Subscription() { // Subscription : 구독 액션을 나타냄
+        @Override
+        public void request(long l) {
+            try {
+                iter.forEach(s -> sub.onNext(s)); // 모든 원소를 순회하며 sub에 보낸다.
+                sub.onComplete(); // 꼭 끝났다고 말을 해줘야 함.
+            } catch (Throwable t) {
+                sub.onError(t);
+            }
+        }
+        @Override
+        public void cancel() {
+
+        }
+    });
+
+}
+```
+
+**Reactive Streams 구현체에서는 `Throwable`이 더 적절합니다:**
+
+1. **완전한 오류 전파**: Reactive Streams 명세에서는 <u>모든 종류의 오류를 downstream으로 전파</u>하도록 권장
+2. **일관성**: RxJava, Project Reactor 등 주요 구현체들이 `Throwable`을 사용
+3. **예상치 못한 오류 처리**: `<u>Error`가 발생해도 스트림이 중단되지 않고 적절히 처리</u>됨
+
+**일반적인 애플리케이션 코드에서는 `Exception`이 더 적절:**
+
+- `Error`는 보통 복구 불가능하므로 잡지 않는 것이 좋음
+- 애플리케이션 레벨의 예외만 처리하는 것이 명확함
+
+따라서 현재 코드에서 `Throwable`을 사용하는 것이 Reactive Streams 패턴에 더 적합합니다.
---
+++ src/main/java/practice/reactiveprograming/PubSub.java
@@ -0,0 +1,152 @@
+package practice.reactiveprograming;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Reactive Streams - Operators : 데이터 가공
+ *
+ * Publisher ---[data]---> Operator ----[data2]----> Operator2 ----[data3]---> SubScriber
+ * 1. map (data1 -> f -> data2)
+ *      : pub ----[data]---> mapPub (op) ----[data2]---> logSub   "downStream"
+ *                                             <-- subscribe(logSub)
+ *                                             --> onSubscribe(s)
+ *                                             --> onNext
+ *                                             --> onNext
+ *                                             --> onComplete
+ */
+@Slf4j
+public class PubSub {
+    public static void main(String[] args) {
+        // Pub : 데이터 제공자
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList()));
+        Publisher<Integer> mapPub = mapPub(pub, s -> s * 10);
+        Publisher<Integer> map2Pub = mapPub(mapPub, s -> s / 5);
+        Publisher<Integer> sumPub = sumPub(pub); // 계산만 하고 있다가 계산 다 하면 그때야 보내는 pub
+        Publisher<Integer> reducePub = reducePub(pub, 0, (a, b) -> a+b); // pub, 초기값, 계산로직
+        // Sub : 데이터 받는자
+        reducePub.subscribe(logSub());
+    }
+
+    private static Publisher<Integer> reducePub(Publisher<Integer> pub, int init, BiFunction<Integer, Integer, Integer> bf) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new DelegateSub(sub) {
+                    int result = init;
+                    @Override
+                    public void onNext(Integer i) {
+                        result = bf.apply(result, i);
+                    }
+                    @Override
+                    public void onComplete() {
+                        sub.onNext(result);
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
+    }
+
+    // reduce 동작 과정 - 초기값 0 [1,2,3,4,5]  ---결과--> 1개
+    // 0 -> (0,1) -> 0 + 1 = 1
+    // 1 -> (1,2) -> 1 + 2 = 3
+    // 3 -> (3,3) -> 3 + 3 = 6 ...
+    private static Publisher<Integer> sumPub(Publisher<Integer> pub) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new DelegateSub(sub) {
+                    int sum = 0;
+                    @Override
+                    public void onNext(Integer i) {
+                        sum += i;
+                    }
+                    @Override
+                    public void onComplete() {
+                        sub.onNext(sum);
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
+    }
+
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new DelegateSub(sub) {
+                    @Override
+                    public void onNext(Integer i) {
+                        sub.onNext(f.apply(i));
+                    }
+                });
+            }
+        };
+    }
+
+    private static Subscriber<Integer> logSub() {
+        return new Subscriber<>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.info("onSubscribe");
+                s.request(Long.MAX_VALUE); //  데이터 일단 이 정도 줘~ (백프레셔 역할 함)
+                //s.cancel();   완료는 아니지만, pub에게 데이터 보내는걸 멈춰줘 할 때 쓴다. sub를 통해 취소를 할 수 있다 정도만 기억.
+            }
+
+            @Override
+            public void onNext(Integer i) {
+                log.info("onNext : {}", i);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.info("onError : {}", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.info("onComplete");
+            }
+
+        };
+    }
+
+    private static Publisher<Integer> iterPub(List<Integer> data) {
+        return new Publisher<>() {
+            // 제공할 데이터 예시
+
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                sub.onSubscribe(new Subscription() { // Subscription : 구독 액션을 나타냄
+                    @Override
+                    public void request(long l) {
+                        try {
+                            data.forEach(s -> sub.onNext(s)); // 모든 원소를 순회하며 sub에 보낸다.
+                            sub.onComplete(); // 꼭 끝났다고 말을 해줘야 함.
+                        } catch (Throwable t) {
+                            sub.onError(t);
+                        }
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+
+            }
+        };
+    }
+
+}
